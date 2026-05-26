cbf488a64a00cb959abccae8fc57cd4d24a1e832
赵庆 <805322803@qq.com>
2026-05-26 11:39:49 +0800

    feat: 静态部署

---
+++ src/types/vite-env.d.ts
@@ -0,0 +1,14 @@
+import Ecs20140526, * as $Ecs20140526 from '@alicloud/ecs20140526'
+import OpenApi, * as $OpenApi from '@alicloud/openapi-client'
+import Util, * as $Util from '@alicloud/tea-util'
+import * as $tea from '@alicloud/tea-typescript'
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_PORT: string
+  readonly VITE_APP_BASE_URL: string
+  readonly VITE_APP_OPEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

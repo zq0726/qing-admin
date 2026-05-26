13070337e64b489b53847fc12c11574191e16226
赵庆 <805322803@qq.com>
2026-05-26 13:51:33 +0800

    feat: 实现静态部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const isGithub = mode === 'github'
+
+  return {
+    base: isGithub ? env.VITE_APP_BASE_URL : '/',
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      host: true,
+      port: +env.VITE_APP_PORT,
+      open: !!env.VITE_APP_OPEN,
+    },
+  }
 })
